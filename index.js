dcd340060829c5f7e7d02c0389a96c2c2c4f5150
Pawelsz20 <98423436+Pawelsz20@users.noreply.github.com>
2022-08-02 16:16:33 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -6,85 +6,85 @@ const sendBtn = document.querySelector(".send");
 const clearBtn = document.querySelector(".clear");
 const popup = document.querySelector(".popup");
 
-const showError = (input, msg) => {
-  const formBox = input.parentElement;
-  const errorMsg = formBox.querySelector(".error-text");
 
-  formBox.classList.add("error");
-  errorMsg.textContent = msg;
-};
+const showError = (input, msg) => {
+  const formBox = input.parentElement
+  const errorMess = formBox.querySelector('.error-text')
+
+  formBox.classList.add('error')
+  errorMess.textContent = msg
+}
 
 const clearError = (input) => {
-  const formBox = input.parentElement;
-  formBox.classList.remove("error");
-};
-
-const checkForm = (input) => {
-  input.forEach((el) => {
-    if (el.value === "") {
-      showError(el, el.placeholder);
-    } else {
-      clearError(el);
-    }
-  });
-};
+  const formBox = input.parentElement
+  formBox.classList.remove('error')
+}
 
 const checkLength = (input, min) => {
-  if (input.value.length < min) {
-    showError( input,`${input.previousElementSibling.innerText.slice(0,-1)} musi składać sie z przynajmniej ${min} znaków.`);
+  if (input.value.length < min && input.value.length !== 0) {
+    showError(input, `${input.previousElementSibling.innerHTML.slice(0, -1)} musi składać sie przynajmniej z ${min} znaków.`)
   }
-};
+}
 
 const checkPass = (pass1, pass2) => {
-  if (pass1.value !== pass2.value) {
-    showError(pass2, 'Hasła do siebie nie pasują');
+  if (pass1.value.length !== pass2.value.length) {
+    showError(pass2, 'Hasła nie są takie same')
   }
-};
-
+}
 
 const checkMail = (email) => {
-  
   const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,3}))$/;
 
+
   if (re.test(email.value)) {
-    clearError(email);
+    clearError(email)
+  } else if (email.value === '') {
+    showError(email, email.placeholder)
   } else {
-    showError(email, 'E-mail jest niepoprawny');
+    showError(email, 'E-mail jest nieprawidłowy')
   }
-};
+}
 
 const showPopup = () => {
-  const allInputs = document.querySelectorAll('.form-box');
-  let errorCount = 0;
+  let count = 0
+  const allInputs = document.querySelectorAll('.form-box')
 
   allInputs.forEach(el => {
     if (el.classList.contains('error')) {
-      errorCount++
+        count++
+      }
+  })
+  
+  if (count === 0) {
+     popup.classList.add('show-popup')
+   }
+}
+
+const checkForm = (input) => {
+  input.forEach(el => {
+    if (el.value === '') {
+      showError(el, el.placeholder)
+    } else {
+      clearError(el)
     }
   })
+}
 
-  if (errorCount === 0) {
-      popup.classList.add('show-popup')
-    }
-};
-
-
-
-
-sendBtn.addEventListener("click", (e) => {
-  e.preventDefault();
-  checkForm([username, pass, pass2, email]);
-  checkLength(username, 3);
-  checkLength(pass, 8);
-  checkPass(pass, pass2);
-  checkMail(email);
+sendBtn.addEventListener('click', (e) => {
+  e.preventDefault()
+  checkForm([username, pass, pass2, email])
+  checkLength(username, 3)
+  checkLength(pass, 8)
+  checkPass(pass, pass2)
+  checkMail(email)
   showPopup()
-});
 
-clearBtn.addEventListener("click", (e) => {
-  e.preventDefault();
-    [username, pass, pass2, email].forEach((el) => {
-      el.value = "";
-      clearError(el);
-  });
-});
+})
+
+clearBtn.addEventListener('click', (e) => {
+  e.preventDefault()
+  const list = [username, pass, pass2, email].forEach(el => {
+    el.value = ''
+    clearError(el)
+  })
+})
